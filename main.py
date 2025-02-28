d4c12bc40a4eb1b09ef94fff340d963d1d2f5a34
Hudson Farias <hudson.farias.dev@gmail.com>
2025-02-28 10:48:17 -0300

    root path

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from os.path import join
 
 load_dotenv()
 
-DOCS_PATH = getenv("DOCS_PATH")
-REDOCS_PATH = getenv("REDOCS_PATH")
+DOCS_PATH = getenv('DOCS_PATH')
+REDOCS_PATH = getenv('REDOCS_PATH')
 
 
-app = FastAPI(docs_url = DOCS_PATH, redoc_url = REDOCS_PATH)
+app = FastAPI(root_path = '/api', docs_url = DOCS_PATH, redoc_url = REDOCS_PATH)
 
-def load(subapp: FastAPI, directory: str = "routers"):
+def load(subapp: FastAPI, directory: str = 'routers'):
     import_cache = {}
 
     for root, _, files in walk(directory):
         for file in files:
-            if not file.startswith("__") and file.endswith(".py"):
-                path = join(root, file).replace(".py", "").replace("/", ".").replace("\\", ".")
+            if not file.startswith('__') and file.endswith('.py'):
+                path = join(root, file).replace('.py', '').replace('/', '.').replace('\\', '.')
 
                 if path not in import_cache:
                     import_cache[path] = import_module(path)
